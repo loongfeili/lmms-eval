7a016d635962f40eac46610bf2b889bc4aa471ea
xiaoyuxi <xiaoyuxi@bytedance.com>
2025-07-23 06:44:17 +0800

    yuxi

--- examples/models/openai_compatible.sh
+++ examples/models/openai_compatible.sh
@@ -2,7 +2,7 @@
 # export https_proxy=http://127.0.0.1:7890
 # MODEL="doubao-1.5-vision-pro-250328"
 MODEL="Qwen/Qwen2.5-VL-72B-Instruct"
-BENCHMARK=""
+BENCHMARK="camerabench_vqa"
 OUTPUT=result/${BENCHMARK}/${MODEL}_results.json
 # export OPENAI_API_KEY="2a1bb43f-1716-4337-9e14-5a3e92eb95ea"
 # export OPENAI_API_BASE="https://ark.cn-beijing.volces.com/api/v3"
@@ -18,7 +18,7 @@ python3 -m lmms_eval \
     --model openai_compatible \
     --model_args model_version=${MODEL},azure_openai=False \
     --tasks ${BENCHMARK}  \
-    --batch_size 1 \
+    --batch_size 5 \
     --output_path ${OUTPUT} \
     --log_samples   \
     --log_samples_suffix _${MODEL}
