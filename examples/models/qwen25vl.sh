07a4b695603419b78884a9c10e18325d2f4654b7
xiaoyuxi <xiaoyuxi@bytedance.com>
2025-07-23 07:01:12 +0800

    yuxi_teask

--- examples/models/qwen25vl.sh
+++ examples/models/qwen25vl.sh
@@ -11,8 +11,8 @@ export HF_HOME="~/.cache/huggingface"
 #     --tasks mmmu_pro \
 #     --batch_size 1
 
-accelerate launch --num_processes=8 --main_process_port=12346 -m lmms_eval \
+python -m accelerate.commands.launch --num_processes=8 --main_process_port=12346 -m lmms_eval \
     --model qwen2_5_vl \
-    --model_args=pretrained=Qwen/Qwen2.5-VL-7B-Instruct,max_pixels=12845056,attn_implementation=flash_attention_2,interleave_visuals=False \
-    --tasks mme \
+    --model_args=pretrained=Qwen/Qwen2.5-VL-7B-Instruct,max_pixels=12845056,fps=3,attn_implementation=flash_attention_2,interleave_visuals=False \
+    --tasks camerabench_vqa \
     --batch_size 1
